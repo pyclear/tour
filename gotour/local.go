5bc6ab64ee42a742f318833072350d3d09b627f6
Pyclearl <pyclear@users.noreply.github.com>
2017-02-20 00:17:52 +0800

    Update local.go
    chang base pkg

--- gotour/local.go
+++ gotour/local.go
@@ -32,13 +32,13 @@ import (
 )
 
 const (
-	basePkg    = "github.com/Go-zh/tour"
+	basePkg    = "github.com/pyclear/tour"
 	socketPath = "/socket"
 )
 
 var (
 	httpListen  = flag.String("http", "0.0.0.0:3999", "host:port to listen on")
-	openBrowser = flag.Bool("openbrowser", true, "open browser automatically")
+	//openBrowser = flag.Bool("openbrowser", true, "open browser automatically")
 )
 
 var (
@@ -179,36 +179,36 @@ func environ() (env []string) {
 
 // waitServer waits some time for the http Server to start
 // serving url. The return value reports whether it starts.
-func waitServer(url string) bool {
-	tries := 20
-	for tries > 0 {
-		resp, err := http.Get(url)
-		if err == nil {
-			resp.Body.Close()
-			return true
-		}
-		time.Sleep(100 * time.Millisecond)
-		tries--
-	}
-	return false
-}
+//func waitServer(url string) bool {
+//	tries := 20
+//	for tries > 0 {
+//		resp, err := http.Get(url)
+//		if err == nil {
+//			resp.Body.Close()
+//			return true
+//		}
+//		time.Sleep(100 * time.Millisecond)
+//		tries--
+//	}
+//	return false
+//}
 
 // startBrowser tries to open the URL in a browser, and returns
 // whether it succeed.
-func startBrowser(url string) bool {
-	// try to start the browser
-	var args []string
-	switch runtime.GOOS {
-	case "darwin":
-		args = []string{"open"}
-	case "windows":
-		args = []string{"cmd", "/c", "start"}
-	default:
-		args = []string{"xdg-open"}
-	}
-	cmd := exec.Command(args[0], append(args[1:], url)...)
-	return cmd.Start() == nil
-}
+//func startBrowser(url string) bool {
+//	// try to start the browser
+//	var args []string
+//	switch runtime.GOOS {
+//	case "darwin":
+//		args = []string{"open"}
+//	case "windows":
+//		args = []string{"cmd", "/c", "start"}
+//	default:
+//		args = []string{"xdg-open"}
+//	}
+//	cmd := exec.Command(args[0], append(args[1:], url)...)
+//	return cmd.Start() == nil
+//}
 
 // prepContent for the local tour simply returns the content as-is.
 func prepContent(r io.Reader) io.Reader { return r }
